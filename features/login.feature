81eb8887e1250ccea6893a56ca176222ec29a0fb
Evvibrevi <evvibrevi@gmail.com>
2025-07-09 19:08:43 +0500

    Add BDD test

--- features/login.feature
+++ features/login.feature
@@ -1,12 +1,23 @@
-Feature: The Internet Guinea Pig Website
+Feature: Login Functionality
 
-  Scenario Outline: As a user, I can log into the secure area
-
+  Scenario: Successful login with valid credentials
     Given I am on the login page
-    When I login with <username> and <password>
-    Then I should see a flash message saying <message>
+    When I enter username "student"
+    And I enter password "Password123"
+    And I click the submit button
+    Then I should be redirected to the success page
+    And I should see a message containing "Congratulations student. You successfully logged in!"
 
-    Examples:
-      | username | password             | message                        |
-      | tomsmith | SuperSecretPassword! | You logged into a secure area! |
-      | foobar   | barfoo               | Your username is invalid!      |
+  Scenario: Login with invalid username
+    Given I am on the login page
+    When I enter username "incorrectUser"
+    And I enter password "Password123"
+    And I click the submit button
+    Then I should see an error message "Your username is invalid!"
+
+  Scenario: Login with invalid password
+    Given I am on the login page
+    When I enter username "student"
+    And I enter password "incorrectPassword"
+    And I click the submit button
+    Then I should see an error message "Your password is invalid!"
--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -1,18 +1,45 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-import { expect, $ } from '@wdio/globals'
+import { expect, $ } from '@wdio/globals';
 
-Given(/^I am on the (\w+) page$/, async (page) => {
-    await browser.url(`https://the-internet.herokuapp.com/${page}`);
+Given('I am on the login page', async () => {
+  await browser.url('https://practicetestautomation.com/practice-test-login/');
 });
 
-When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-    await $('#username').setValue(username);
-    await $('#password').setValue(password);
-    await $('button[type="submit"]').click();
+When('I enter username {string}', async (username: string) => {
+  await $('#username').setValue(username);
 });
 
-Then(/^I should see a flash message saying (.*)$/, async (message) => {
-    await expect($('#flash')).toBeExisting();
-    await expect($('#flash')).toHaveText(expect.stringContaining(message));
+When('I enter password {string}', async (password: string) => {
+  await $('#password').setValue(password);
 });
 
+When('I click the submit button', async () => {
+  await $('#submit').click();
+});
+
+Then('I should be redirected to the success page', async () => {
+  await browser.waitUntil(
+    async () => (await browser.getUrl()).includes('logged-in-successfully'),
+    {
+      timeout: 5000,
+      timeoutMsg: 'Expected to be redirected to the success page',
+    }
+  );
+});
+
+Then('I should see a message containing {string}', async (text: string) => {
+  const message = await $('.has-text-align-center strong');
+  const actual = (await message.getText()).toLowerCase();
+  expect(actual).toContain(text.toLowerCase());
+});
+
+Then('the {string} button should be visible', async (text: string) => {
+  const button = await $(`button*=${text}`);
+  expect(await button.isDisplayed()).toBe(true);
+});
+
+Then('I should see an error message {string}', async (errorMsg: string) => {
+  const errorElement = await $('#error');
+  expect(await errorElement.isDisplayed()).toBe(true);
+  expect(await errorElement.getText()).toBe(errorMsg);
+});
